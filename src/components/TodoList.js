9bfe52794b5f9991543461e48858382b9f19dac4
Jonathan Perry <jpper@umich.edu>
2020-10-29 15:40:09 +0900

    Added in functionality to delete a todo when clicking on the trash button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,19 @@
 import React from "react";
 // Import Components
-const TodoList = () => {
+import Todo from "./Todo";
+const TodoList = ({ todos, setTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+          />
+        ))}
       </ul>
     </div>
   );
