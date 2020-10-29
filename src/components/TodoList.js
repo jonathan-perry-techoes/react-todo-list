85536d3f4f8adc45206dccd1e90bb6b378389af4
Jonathan Perry <jpper@umich.edu>
2020-10-29 14:51:54 +0900

    Passing down state from app into the form, and implemented pushing setting todos on form submit

---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+// Import Components
+const TodoList = () => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
